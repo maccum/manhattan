9565370db23053c42fb5b07573545f1ac14ffcdb
Meredith Accum <meredith.accum@gmail.com>
2018-06-26 15:37:21 -0400

    wip

---
+++ generator/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tile_generator',
+      version='0.1',
+      description='plot generator for leaflet tiles',
+      url='',
+      author='meredith accum',
+      author_email='',
+      license='',
+      packages=['generator'],
+      zip_safe=False)
