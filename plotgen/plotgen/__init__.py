d9ef6fd87935af733059000bcac50a1274f182ae
Meredith Accum <meredith.accum@gmail.com>
2018-07-03 15:04:05 -0400

    deleted plotgen folder (replaced by slippyplot)

--- plotgen/plotgen/__init__.py
+++
@@ -1,8 +0,0 @@
-from .src.gen.gen import PlotGenerator
-from .src.utils.zone import Zone, Zones
-
-__all__ = [
-    'PlotGenerator',
-    'Zones',
-    'Zone'
-]
--- plotgen/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='plotgen',
-    version='0.1dev',
-    author='meredith accum',
-    packages=['plotgen'],
-    description='plot generator for manhattan plot tiles for leaflet',
-)
