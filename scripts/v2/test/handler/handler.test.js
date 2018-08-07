311854b1c66db3c498b14e47058a438bc2ad60f2
Meredith Accum <meredith.accum@gmail.com>
2018-08-07 15:00:44 -0400

    wip

---
+++ scripts/v2/test/handler/handler.test.js
@@ -0,0 +1,9 @@
+var assert = require('chai').assert;
+
+describe('Handler', function () {
+    describe('Dragging', function () {
+        it('listenForDrag should be called when <svg>#plot is loaded', function () {
+
+        });
+    });
+});
